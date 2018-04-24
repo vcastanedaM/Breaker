5e883e2d6475b7883f8ad728bf69585a0014fda1
Victor Castaneda <victorcastaneda@victors-mac.local>
2018-04-24 11:17:19 -0500

    added comment

--- Breaker/GameScene.swift
+++ Breaker/GameScene.swift
@@ -4,86 +4,83 @@
 //
 //  Created by Victor Castaneda on 4/21/18.
 //  Copyright © 2018 Victor Castaneda. All rights reserved.
-//
+// Victor Castaneda
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball:SKSpriteNode!
+    var paddle: SKSpriteNode!
+    var brick = SKSpriteNode()
+    var label = UILabel()
     
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "Ball") as! SKSpriteNode
+        paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        ball.physicsBody?.applyImpulse(CGVector(dx: 45, dy: 50))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
+        self.physicsWorld.contactDelegate = self
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches{
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches{
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyAname = contact.bodyA.node?.name
+        let bodyBname = contact.bodyB.node?.name
+        
+        
+        if bodyAname == "Ball" && bodyBname == "Brick" || bodyAname == "Brick" && bodyBname == "Ball" {
+            if bodyAname == "Brick" {
+                contact.bodyA.node?.removeFromParent()
+            } else if bodyBname == "Brick" {
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
+            if bodyAname == "Ball" && bodyBname == "BrickTwo" || bodyAname == "BrickTwo" && bodyBname == "Ball" {
+            if bodyAname == "BrickTwo" {
+                contact.bodyA.node?.removeFromParent()
+            }
+            else if bodyBname == "BrickTwo" {
+                contact.bodyB.node?.removeFromParent()
+            }
+            }
+            if bodyAname == "Ball" && bodyBname == "BrickThree" || bodyAname == "BrickThree" && bodyBname == "Ball"{
+            if bodyAname == "BrickThree" {
+                contact.bodyA.node?.removeFromParent()
+            }
+            else if bodyBname == "BrickThree"{
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
+        if bodyAname == "Ball" && bodyBname == "BrickFour" || bodyAname == "BrickFour" && bodyBname == "Ball" {
+            if bodyAname == "BrickFour" {
+                contact.bodyA.node?.removeFromParent()
+            }
+            else if bodyBname == "BrickFour"{
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
+        if bodyAname == "Ball" && bodyBname == "loseZone" || bodyAname == "loseZone" && bodyBname == "Ball"{
+            if bodyAname == "Ball"{
+                contact.bodyA.node?.removeFromParent()
+            }
+        }
 }
+    
+}
+
